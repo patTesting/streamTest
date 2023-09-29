f277f524d0ab2edd6cdd96b4803a2401ef99e7da
a <a@a.aaa>
2023-09-29 21:56:29 +0200

    a

--- lib.js
+++ lib.js
@@ -30373,7 +30373,7 @@ async function requestBasicPermissions(constraint = {video: true, audio: true}, 
 							} else if (ChromiumVersion && !session.mobile){
 								warnUser("<h1>Camera/mic permissions denied</h1>\nPlease ensure you have allowed the mic/camera permissions in your browser, such as like:\n\n<img src='./media/permissions_chrome.jpg' />.", false, false);
 							} else {
-								warnUser("Permission access to the camera or microphone was denied.\n\nPlease ensure you have allowed the mic/camera permissions in your browser.\n\nFor guides on how to resolve this issue, please refer to:\n\n<a target='_blank' href='https://docs.vdo.ninja/common-errors-and-known-issues/enable-camera-microphone-permissions'>https://docs.vdo.ninja/common-errors-and-known-issues/enable-camera-microphone-permissions</a>.", false, false);
+								warnUser("Permission access to the camera or microphone was denied.\n\nPlease ensure you have allowed the mic/camera permissions in your browser.", false, false);
 							}
 						}, 1);
 					}
